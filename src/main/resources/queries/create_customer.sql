5188582b2bd4a1d18ea1dc6263530845635c36e4
Jeff <Jeff>
2017-03-31 16:56:34 -0400

    add table creation sql

---
+++ src/main/resources/queries/create_customer.sql
@@ -0,0 +1,6 @@
+CREATE TABLE Customer(
+	user_id int, 
+	pref_store_id int,
+	primary key(user_id)
+	foreign key(user_id) references App_User(user_id),
+	foreign key(pref_store_id) references Store(store_id));
