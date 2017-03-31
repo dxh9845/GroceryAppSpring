e04bf7072929dc2dc9f4036b815c3b5e04a7e059
Zach Glassner <zach.glassner@gmail.com>
2017-03-31 16:56:58 -0400

    add Zach's SQL Tables

---
+++ src/main/resources/queries/role.sql
@@ -0,0 +1,5 @@
+CREATE TABLE Role(
+  role_id integer NOT NULL,
+  role_desc varchar(255),
+  PRIMARY KEY(role_id)
+);
---
+++ src/main/resources/queries/shopping_cart.sql
@@ -0,0 +1,7 @@
+CREATE TABLE Shopping_Cart(
+  customer_id integer NOT NULL,
+  product_id integer NOT NULL,
+  PRIMARY KEY(customer_id, product_id),
+  FOREIGN KEY(customer_id) REFERENCES Customer(user_id),
+  FOREIGN KEY(product_id) REFERENCES Product(product_id)
+);
