7b8afcbbcd697968377ff13f301a661f7f3fc27e
Daniel Herzig <dxh9845@rit.edu>
2017-03-31 16:29:47 -0400

    Added initial SQL file in resources.

---
+++ src/main/resources/queries/create_store.sql
@@ -0,0 +1,3 @@
+CREATE TABLE store( store_id int, name varchar(25),
+                                       location varchar(50),
+                                                PRIMARY KEY (store_id));
