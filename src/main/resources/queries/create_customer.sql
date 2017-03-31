344d8a30a4fad04ea609c99ec67680dc72197abe
Daniel Herzig <dxh9845@rit.edu>
2017-03-31 17:40:29 -0400

    Deleted old queries.

--- src/main/resources/queries/create_customer.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE Customer(
-	user_id int, 
-	pref_store_id int,
-	primary key(user_id)
-	foreign key(user_id) references App_User(user_id),
-	foreign key(pref_store_id) references Store(store_id));
--- src/main/resources/queries/create_role.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE Role(
-  role_id integer NOT NULL,
-  role_desc varchar(255),
-  PRIMARY KEY(role_id)
-);
