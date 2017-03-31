5639a6f1d40a3f51d5e145bac09fd72c20d23935
Daniel Herzig <dxh9845@rit.edu>
2017-03-31 16:13:45 -0400

    Initial commit

---
+++ src/main/java/desbytes/controllers/IndexController.java
@@ -0,0 +1,18 @@
+package desbytes.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * Created by danie on 3/15/2017.
+ */
+@Controller
+public class IndexController {
+    @RequestMapping("/")
+    public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "index";
+    }
+}
