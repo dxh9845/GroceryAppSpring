56e7cbd1c2adb504e2c6fbb5fc760024b40899e9
Daniel Herzig <dxh9845@rit.edu>
2017-03-31 17:39:28 -0400

    Changed query name to reflect we are creating tables not actual tuples.

---
+++ src/main/resources/queries/create_product_table.sql
@@ -0,0 +1,5 @@
+CREATE TABLE PRODUCT(
+	product_id int NOT NULL, 
+	name varchar(50), 
+	price numeric(5, 2), 
+	PRIMARY KEY (product_id));
---
+++ src/main/resources/queries/create_store_tabl;e.sql
@@ -0,0 +1,5 @@
+CREATE TABLE Store(
+	store_id int NOT NULL, 
+	name varchar(25), 
+	location varchar(50), 
+	PRIMARY KEY (store_id));
