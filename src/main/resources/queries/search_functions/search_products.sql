60df20cd5d8059893d68cadea289441bb7444116
Daniel Herzig <dxh9845@rit.edu>
2017-04-08 17:49:21 -0400

    Modified to use updated column names and tested in H2.

--- src/main/resources/queries/search_functions/search_products.sql
+++ src/main/resources/queries/search_functions/search_products.sql
@@ -1,25 +1,17 @@
-CREATE PROCEDURE search_products(
-	@searchTerm varchar(50),
-	@preferredStoreID INT
-)
-AS 
-BEGIN 
-	SELECT 
-		product.product_id
-		product.name,
-		product.price,
-		store.name,
-		inventory.count,
-		inventory.aisle
-		FROM 
-			inventory, product, store
-		WHERE 
-			inventory.product_id = product.product_id
-			AND
-			inventory.store_ID = @preferredStoreID
-			AND 
-			store.store_id = store.store_id
-			AND
-			@searchTerm LIKE product.name
-
-END
+SELECT 
+	product.product_id,
+	product.name,
+	product.price,
+	store.name,
+	inventory.qty,
+	inventory.aisle
+	FROM 
+		inventory, product, store
+	WHERE 
+		inventory.product_id = product.product_id
+		AND
+		inventory.store_ID = ??
+		AND 
+		inventory.store_id = store.store_id
+		AND
+		?? LIKE product.name
