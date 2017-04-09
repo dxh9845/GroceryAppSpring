20f35923539cab1ef1532c834328337ccb27bdbf
Zach Glassner <zach.glassner@gmail.com>
2017-04-09 17:32:30 -0400

    fixed get_order_history for H2

--- src/main/resources/queries/get_order_history.sql
+++ src/main/resources/queries/get_order_history.sql
@@ -1,22 +1,26 @@
 -- Gets all the dates of order in desc order
+
 SELECT Grocery_Order.order_time
 FROM Grocery_Order
 GROUP BY Grocery_Order.order_time
 ORDER BY Grocery_Order.order_time DESC;
 
 -- Combines Order_Product with its foreign keys
+
 CREATE VIEW Order_Info AS
-SELECT Order_Product.count,
+SELECT Order_Product.qty,
        Product.price,
        Product.name,
        Grocery_Order.order_time
-FROM Order_Product
-JOIN Product ON Order_Product.product_id = Product.product_id
-JOIN Grocery_Order ON Order_Product.order_id = Grocery_Order.order_id
+FROM Order_Product,
+     Product,
+     Grocery_Order
+WHERE Order_Product.product_id = Product.product_id
+    AND Order_Product.order_id = Grocery_Order.order_id
 ORDER BY Grocery_Order.order_time DESC;
 
 -- Gets all order items for a certain date
-SELECT Product.Name,
-       Order_Product.qty,
+
+SELECT *
 FROM Order_Info
 WHERE Order_Info.order_time = ?;
