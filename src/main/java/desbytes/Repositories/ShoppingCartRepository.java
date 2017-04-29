f58191d1a2d7f172fbb5d5537664b072b8969b46
Zane Grasso <ztg6893@g.rit.edu>
2017-04-29 14:54:23 -0400

    shoppingcart repository

--- src/main/java/desbytes/Repositories/ShoppingCartRepository.java
+++ src/main/java/desbytes/Repositories/ShoppingCartRepository.java
@@ -1,8 +1,49 @@
 package desbytes.Repositories;
+import com.oracle.javafx.jmx.SGMXBeanImpl;
+import desbytes.models.Shopping_Cart;
+import desbytes.utils.QueryReader;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.PreparedStatementCreator;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+
+import javax.annotation.PostConstruct;
+import javax.sql.DataSource;
+import java.sql.*;
+import javax.sql.*;
+import java.util.List;
 
 /**
  * Created by zanegrasso
  * Created on 4/27/17.
  */
 public class ShoppingCartRepository {
+
+    private JdbcTemplate jdbcTemplate;
+
+    @SuppressWarnings("SpringJavaAutowiringInspection")
+    @Autowired
+    public void setDataSource(DataSource dataSource) {
+        this.jdbcTemplate = new JdbcTemplate(dataSource);
+    }
+
+    @PostConstruct
+    public Shopping_Cart getShoppingCartByID(int id)
+    {
+        QueryReader reader = new QueryReader();
+        String content = reader.readQueryFile("queries", "get_shopping_cart.sql");
+        return jdbcTemplate.queryForObject(content, new Object[]{id}, new ShoppingCartRowMapper());
+    }
+
+    public class ShoppingCartRowMapper implements RowMapper<Shopping_Cart> {
+        @Override
+        public Shopping_Cart mapRow(ResultSet rs, int rowNum) throws SQLException {
+            int customer_id = rs.getInt("customer_id");
+            double product_id  = rs.getDouble("product_id");
+            int qty = rs.getInt("qty");
+            return new Shopping_Cart(customer_id, product_id, qty);
+        }
+    }
 }
