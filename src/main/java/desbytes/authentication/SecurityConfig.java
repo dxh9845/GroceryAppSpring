3a57033726cec7d7524f60d4a3e0b720bbb3359c
Daniel Herzig <dxh9845@rit.edu>
2017-05-02 00:18:16 -0400

    Wrote login / logout functionality.

---
+++ src/main/java/desbytes/authentication/SecurityConfig.java
@@ -0,0 +1,55 @@
+package desbytes.authentication;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+import javax.sql.DataSource;
+
+/**
+ * Configure the login authentication.
+ */
+@Configuration
+@EnableAutoConfiguration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    DataSource dataSource;
+
+    @SuppressWarnings("SpringJavaAutowiringInspection")
+    @Autowired
+    public void setDataSource(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/register").permitAll()
+                .antMatchers("/").permitAll()
+                // Fix for h2 console
+                .antMatchers("/css/**").permitAll()
+                .antMatchers("/console/**").permitAll()
+                .antMatchers("/search").permitAll();
+        http
+                .formLogin().loginPage("/login").failureUrl("/login-error")
+                .usernameParameter("username").passwordParameter("password")
+                .and()
+                .logout().logoutSuccessUrl("/login?logout");
+        http
+                .csrf().disable();
+        http.headers().frameOptions().disable();
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {auth.jdbcAuthentication().dataSource(dataSource)
+                .usersByUsernameQuery("SELECT username, password, 'true' as enabled from APP_USER where username=?")
+                .authoritiesByUsernameQuery("SELECT username, role_id from APP_USER WHERE username=?");
+    }
+}
--- src/main/java/desbytes/controllers/IndexController.java
+++ src/main/java/desbytes/controllers/IndexController.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
@@ -36,5 +37,22 @@ public class IndexController {
         model.addAttribute("name", name);
         model.addAttribute("productList", productList);
         return "index";
+    }
+
+    @RequestMapping(value = "/login")
+    public String login() {
+        return "login";
+    }
+
+    @RequestMapping(value = "/login", method = RequestMethod.POST)
+    public String postLogin() {
+        // TODO Enable form login with Spring Security (trigger error for now)
+        return "login";
+    }
+
+    @RequestMapping("/login-error")
+    public String loginError(Model model) {
+        model.addAttribute("loginError", true);
+        return "login";
     }
 }
